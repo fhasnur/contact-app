e0fb4ebf1fc91b65fc4f387f5549abc8cbee12db
Fandi Hasnur <fandi.hasnur10@gmail.com>
2021-08-18 10:31:14 +0800

    install express package
    install express package and add some files

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const renderHTML = (path, res) => {
-    fs.readFile(path, (err, data) => {
-        if(err) {
-            res.writeHead(404);
-            res.write('Error: file not found');
-        } else {
-            res.write(data);
-        }
-        res.end();  
-    })
-}
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', {root:__dirname})
+})
 
-http
-    .createServer((req, res) => {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-        });
+app.get('/about', (req, res) => {
+    res.sendFile('about.html', {root:__dirname})
+})
 
-        const url = req.url;
+app.get('/contact', (req, res) => {
+    res.sendFile('contact.html', {root:__dirname})
+})
 
-        if (url === '/about') {
-            renderHTML('./about.html', res);
-        } else if (url === '/contact') {
-            renderHTML('./contact.html', res);
-        } else {
-            /* res.write('Hello World'); */
-            renderHTML('./index.html', res);
-        }        
-    })
-    .listen(port, () => {
-        console.log('Server is listening on port 3000..');
-    })
+app.get('/product/:id/', (req, res) => {
+    res.send(`Product ID : ${req.params.id} <br> Category : ${req.query.category}`)
+} )
+
+// menjalankan sebuah midleware
+app.use('/', (req, res) => {
+    res.status(404)
+    res.send('<h1>404</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+
+
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const port = 3000;
+
+// const renderHTML = (path, res) => {
+//     fs.readFile(path, (err, data) => {
+//         if(err) {
+//             res.writeHead(404);
+//             res.write('Error: file not found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();  
+//     })
+// }
+
+// http
+//     .createServer((req, res) => {
+//         res.writeHead(200, {
+//             'Content-Type': 'text/html',
+//         });
+
+//         const url = req.url;
+
+//         if (url === '/about') {
+//             renderHTML('./about.html', res);
+//         } else if (url === '/contact') {
+//             renderHTML('./contact.html', res);
+//         } else {
+//             /* res.write('Hello World'); */
+//             renderHTML('./index.html', res);
+//         }        
+//     })
+//     .listen(port, () => {
+//         console.log('Server is listening on port 3000..');
+//     })
