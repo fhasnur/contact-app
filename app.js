d1d4f3ca1dd1b4704ca8da0796bc598939b03bf6
Fandi Hasnur <fandi.hasnur10@gmail.com>
2021-08-23 16:23:40 +0800

    edit file
    edit file

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-const morgan = require('morgan')
+const {loadContact, findContact} = require('./utils/contacts')
 const app = express()
 const port = 3000
 
-// gunakan ejs
+
 app.set('view engine', 'ejs')
-
-// third-party middleware
-app.use(expressLayouts)
-app.use(morgan('dev'))
-
-// built-in middleware
-app.use(express.static('public'))
-
-// application level middleware
-app.use((req, res, next) => {
-  console.log('Time', Date.now())
-  next()
-})
+app.use(expressLayouts) // third-party middleware
+app.use(express.static('public')) // built-in middleware
 
 app.get('/', (req, res) => {
-  /* res.sendFile('./index.html', {root:__dirname}) */
   const mahasiswa = [
     {
       nama: 'Fandi Hasnur',
@@ -52,17 +40,25 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/contact', (req, res) => {
+  const contacts = loadContact()
+
   res.render('contact', { 
     layout: 'layouts/main-layout',
     title: 'Halaman Contact',
+    contacts
   })
 })
 
-app.get('/product/:id/', (req, res) => {
-    res.send(`Product ID : ${req.params.id} <br> Category : ${req.query.category}`)
-} )
+app.get('/contact/:nama', (req, res) => {
+  const contact = findContact(req.params.nama)
 
-// menjalankan sebuah midleware
+  res.render('detail', { 
+    layout: 'layouts/main-layout',
+    title: 'Halaman Detail Contact',
+    contact
+  })
+})
+
 app.use('/', (req, res) => {
     res.status(404)
     res.send('<h1>404</h1>')
